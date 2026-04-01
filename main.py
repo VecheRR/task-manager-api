735d00240a54f9de77ed1356b2e580fe409abf63
vecher <vecherkovsky2003@mail.ru>
2026-04-01 20:13:22 +0300

    Сделал endpoint 'delete'. Все работает

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
-from uuid import uuid4
 
 app = FastAPI()
+
+next_id = 1
 
 tasks = [
     {"id": 1, "title": "Купить молоко"},
@@ -18,7 +19,7 @@ def get_task(task_id: int):
     for task in tasks:
         if task["id"] == task_id:
             return task
-    return {"error": "Task not found"}
+    raise HTTPException(status_code=404, detail="Task not found")
 
 class TaskReq(BaseModel):
     title: str = Field(..., )
@@ -27,8 +28,19 @@ class Task(BaseModel):
     id: int
     title: str = Field(..., )
 
-@app.post("/tasks")
+@app.post("/tasks", status_code=201)
 def new_task(taskReq: TaskReq):
-    task = Task(id=len(tasks) + 1, title=taskReq.title)
+    global next_id
+    task = Task(id=next_id, title=taskReq.title)
+    next_id += 1
     tasks.append(task.model_dump())
     return task
+
+@app.delete("/tasks/{task_id}")
+def del_task(task_id: int):
+    for i in range(len(tasks)):
+        if tasks[i]["id"] == task_id:
+            del tasks[i]
+            return {"message": "Task deleted successfully"}
+    else:
+        raise HTTPException(status_code=404, detail="Task not found")
